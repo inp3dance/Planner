20637eeea425e0de0f371566d945b88bcfeb4e34
Zoltan Ilyes <zoltan.ilyes@gmail.com>
2017-10-11 01:23:11 +0200

    Dev day 1, some models, empty UI project.

---
+++ Models/ScheduledTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Models
+{
+    public class ScheduledTask
+    {
+        public int Id { get; set; }
+        public int TaskId { get; set; }
+        public int DateId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+        public List<string> Checklist { get; set; }
+        public int TimeEstimate { get; set; }
+        public int ParentId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
